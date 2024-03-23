d8afd7e4e7bb5ad644f636bf82c439e379b4c99f
Noah Chirchir <chirchirnoah37@gmail.com>
2024-03-23 14:09:39 +0300

    Modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+const employee = {
+    name: 'Noah chirchir',
+    age: 20,
+    department: 'Dev'
+};
+
+
+updateEmployeeWithKeyAndValue(employee, 'position', 'Manager');
+
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+
+function deleteFromEmployeeByKey(employee, key) {
+    const clone = { ...employee };
+    delete clone[key];
+    return clone;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
+
